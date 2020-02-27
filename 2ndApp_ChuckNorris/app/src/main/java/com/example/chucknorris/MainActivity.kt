4b9c4d39f9a63fe8e0d9df8f48f7f925dc552a10
lamyvert <tiphanie.lamyverdin@edu.esiee.fr>
2020-02-27 10:52:25 +0100

    RecylerView Instantiation

--- 2ndApp_ChuckNorris/app/src/main/java/com/example/chucknorris/MainActivity.kt
+++ 2ndApp_ChuckNorris/app/src/main/java/com/example/chucknorris/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.chucknorris
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +14,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val list = ListOfJokes.list
+        //Log.i("logcat",list.toString())
 
-        Log.i("logcat",list.toString())
+        val viewManager = LinearLayoutManager(this)
+        val recyclerView = findViewById<RecyclerView>(R.id.RecyclerView)
+        
+
+
     }
 }
